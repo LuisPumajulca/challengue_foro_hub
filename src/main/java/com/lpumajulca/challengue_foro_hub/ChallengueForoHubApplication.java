4cc3267960ee9b616b0308f06663b7b1db17510d
LuisPumajulca <pumajulca@hotmail.com>
2024-07-07 11:58:17 -0500

    first commit

---
+++ src/main/java/com/lpumajulca/challengue_foro_hub/ChallengueForoHubApplication.java
@@ -0,0 +1,13 @@
+package com.lpumajulca.challengue_foro_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengueForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengueForoHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lpumajulca/challengue_foro_hub/ChallengueForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpumajulca.challengue_foro_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengueForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
